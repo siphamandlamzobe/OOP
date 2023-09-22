5dffbb4a4bb9437806c6a89e569f1f641dbed75e
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2023-09-22 09:02:35 +0200

    Default access modifiers

---
+++ DefaultAccessModifiers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultAccessModifiers
+{
+    class ClassAtNamespaceLevel //internal
+    {
+        int number; //private
+        class NestedClass //private by default
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
