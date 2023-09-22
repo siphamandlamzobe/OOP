b0d9d107b8654250cffed40fbca39818cb3e3cfe
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2023-09-22 10:00:33 +0200

    Params key word

---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ParamsKeyword
+{
+    internal class Program
+    {
+        static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(1, 2, 3, 4));
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
