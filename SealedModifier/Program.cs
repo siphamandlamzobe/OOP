5cd830a969e47d900ee52afa66e46ab4ef86d519
Siphamandla Mzobe <Siphamandla.Mzobe@kcpivot.co.za>
2023-09-22 09:41:35 +0200

    Sealed modifiers

---
+++ SealedModifier/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SealedModifier;
+
+public class SealedBase
+{
+    public virtual void DoSomething()
+    {
+        Console.WriteLine("Base Class");
+    }
+}
+
+public class Derived : SealedBase
+{
+    public override sealed void DoSomething()
+    {
+        Console.WriteLine("Child Class");
+    }
+}
+
+public class DerivedFromDerived : Derived
+{
+    public override void DoSomething() //can not override a sealed override method
+    {
+        Console.WriteLine("Chikd Class");
+    }
+}
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
